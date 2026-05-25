8b87758f2bb6060757aef70894f6935f707c23ed
menoo-o <menosuper6@gmail.com>
2026-05-25 13:59:06 +0500

    progress made pt8

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,14 +12,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           rel="stylesheet"
+
+          
         />
-      </head>
+      </head> */}
       <body>
       <Nav />
 
